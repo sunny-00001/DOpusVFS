5745f2c485661f2011acd9de71a0d667bccf136a
sunny-00001 <sunny-00001@users.noreply.github.com>
2026-05-30 22:57:02 +0800

    Clean up redundant files: remove .bak, .workbuddy, backup resource files, move docs to docs/

--- Download/PretentiousName/PNOpusPlugins_2009-08-03/dcrawrap/ReaderPPM.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace ReaderPNM
-{
-	// The Win32IOWrapper does not need to support seeking.
-	// The pVPFileInfo and phBitmap parameters can be NULL if not wanted.
-	bool Process(Win32IOWrapper *pIO, CRITICAL_SECTION *pReadCS, LPVIEWERPLUGINFILEINFOW lpVPFileInfo, HBITMAP *phBitmap);
-};
